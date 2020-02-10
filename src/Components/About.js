dc012ec309cb6ba6d1a98c728f1bbe268389f916
Adam <adamcpenman@gmail.com>
2020-02-10 17:00:05 -0600

    added another project, added more in about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,8 +10,10 @@ function About() {
                     {/* <h2> About Me</h2> */}
                     <p>Hello, Hello, Hello! My name is Adam Penman. Pronouns are 'He/Him/His'. I am a full-stack developer and a magical being. <br/>
                         I have learned various different langaugaes and libraries, but constantly reviewing and 
-                        learning new ways to write code every day! I have attended Lambda School to 
-                        get an excellent base and grasp on concepts. Feel Free to contact me with any questions! 
+                        learning new ways to write code every day. I have attended Lambda School to 
+                        get an excellent base and grasp on concepts. Currently, searching for the right full-time role as a 
+                        Software Developer with an awesome company. I want to be able to create dynamic, user friendly content while
+                        having the necessary tools to continue to grow and harness my skills as a Software Developer!  Feel Free to contact me with any questions! 
                     </p> 
                 </div>
             </div>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -18,6 +18,7 @@ function Projects() {
               <li>Created Jest Tests for each endpoint </li>
               <li>Restricted paths unless a user is logged in.</li>
               </ul>
+               <a href="/">GitHub Repo</a>
             </div>
           </div>
         </div>
@@ -39,16 +40,23 @@ function Projects() {
          <div className="TopDiv3">
           <div className="MainDiv3">
             <div className="BabyDiv3">
-              <h2> Project 1</h2>
-              <p>Project one</p>
+                <h2> Dark-Mode</h2>
+              <h4>React | Axios | Local Storage</h4>
+              <ul>
+                <li>Implemented the usage of React Hooks while creating components</li>
+                <li>Created a custom hook to store data in local storage</li>
+                <li>Created a dark-mode component to toggle between light/dark</li>
+                <li>Axios calls are being made to receive data from a web API</li>
+              </ul>
+              <a href="/">GitHub Repo</a>
             </div>
           </div>
         </div>
          <div className="TopDiv4">
           <div className="MainDiv4">
             <div className="BabyDiv4">
-              <h2> Project 1</h2>
-              <p>Project one</p>
+              <h2> GitHub Repos</h2>
+              <p>Please take a look at my other various projects, code, and the rest of my repos on <a href="/">GitHub</a></p>
             </div>
           </div>
         </div>
