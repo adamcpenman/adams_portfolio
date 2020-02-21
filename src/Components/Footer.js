317a8ec3b02c5ef5128a18f438e6fd5c18276c36
Adam <adamcpenman@gmail.com>
2020-02-21 14:55:04 -0600

    reorganized scss files and used react-reveal library '

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,25 +6,33 @@ import { LinkedinSquare } from 'styled-icons/boxicons-logos/LinkedinSquare';
 import { Instagram } from 'styled-icons/boxicons-logos/Instagram';
 import { Spotify } from 'styled-icons/boxicons-logos/Spotify';
 
-const Div = styled.div`
-text-align: center;
-margin: 5px;
-`
 const A = styled.a`
-color: black;
-`
+  color: black;
+`;
 
 function Footer() {
-    return (
-        <Div>
-            <A href="http://www.facebook.com/adambullet" target="_blank"><FacebookSquare className="footer-icons" /></A>
-            <A href="https://github.com/adamcpenman" target="_blank"><Github className="footer-icons" /></A>
-            <A href="https://www.linkedin.com/in/adamcpenman/" target="_blank"><LinkedinSquare className="footer-icons" /></A>
-            <A href="https://www.instagram.com/adambullet/" target="_blank"><Instagram className="footer-icons" /></A>
-            <A href="https://open.spotify.com/playlist/22J8LlcJBk3Qmd0m07dw8T?si=R6fjSZD5R-Cyj7HdziPsPg" target="_blank"><Spotify className="footer-icons" /></A>
-            <h5>Adam Penman ©2019</h5>
-        </Div>
-    )
+  return (
+    <div className="footerDiv">
+      <A href="http://www.facebook.com/adambullet" target="_blank">
+        <FacebookSquare className="footer-icons" />
+      </A>
+      <A href="https://github.com/adamcpenman" target="_blank">
+        <Github className="footer-icons" />
+      </A>
+      <A href="https://www.linkedin.com/in/adamcpenman/" target="_blank">
+        <LinkedinSquare className="footer-icons" />
+      </A>
+      <A href="https://www.instagram.com/adambullet/" target="_blank">
+        <Instagram className="footer-icons" />
+      </A>
+      <A
+        href="https://open.spotify.com/playlist/22J8LlcJBk3Qmd0m07dw8T?si=R6fjSZD5R-Cyj7HdziPsPg"
+        target="_blank">
+        <Spotify className="footer-icons" />
+      </A>
+      <h5>Adam Penman ©2019</h5>
+    </div>
+  );
 }
 
 export default Footer;
